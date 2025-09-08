a5c7bc60f39fc4a957bb9e9aa855c77840f438bd
Eneida Anzora <anzoraeneida@outlook.com>
2025-09-08 15:14:02 -0600

    Se crearon las interfaces de AuthService y UsuarioRepository además de instalo el paquete nuget pomelo

---
+++ AuthApi/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+using AuthApi.DTOs.UsuarioDTOs;
+
+namespace AuthApi.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<UsuarioRespuestaDto> RegistrarAsync(UsuarioRegistroDTO dto);
+        Task<UsuarioRespuestaDto> LoginAsync(UsuarioLoginDto dto);
+    }
+}
---
+++ AuthApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+using AuthApi.DTOs.UsuarioDTOs;
+using AuthApi.Entidades;
+
+namespace AuthApi.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> GetByEmailAsync(string email);
+        Task<Usuario> AddAsync(Usuario usuario);
+        Task<List<UsuarioListadoDto>> GetAllUsuarioAsync();
+    }
+}
