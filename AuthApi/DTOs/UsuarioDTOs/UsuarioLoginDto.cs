51fa217ea5f787f0436f083238981aca4bf75ad0
Eneida Anzora <anzoraeneida@outlook.com>
2025-09-08 14:30:24 -0600

    Se crearon los Dto para la entidad Usuario

---
+++ AuthApi/DTOs/UsuarioDTOs/UsuarioLoginDto.cs
@@ -0,0 +1,9 @@
+namespace AuthApi.DTOs.UsuarioDTOs
+{
+    public class UsuarioLoginDto
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        
+    }
+}
